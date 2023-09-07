051506271eb7cf229e2d869ba735e343e8ef2f7c
Brendon Thiede <brendon.thiede@gmail.com>
2023-09-07 02:14:57 -0400

    Adding script of usefulness

---
+++ scripts/Get-DbCreds.ps1
@@ -0,0 +1,15 @@
+$DBConfig = "$((Get-ChildItem -Directory C:\fv\*\*\Filevine.WindowsService.FilevineService\* | Sort-Object -Property CreationTime -Descending | Select-Object -First 1).FullName)\Filevine.Data.EF.dll.config"
+
+[xml]$xml = Get-Content -Path $DBConfig
+
+$ConnectionString = ($xml.SelectSingleNode("//configuration/connectionStrings/add[@name='FilevineContext']")).connectionString
+
+$DBServer = ($ConnectionString -replace ';', "`n" | Select-String -Pattern 'data source=(.*)' -AllMatches).Matches[0].Groups[1].Value
+$DBName = ($ConnectionString -replace ';', "`n" | Select-String -Pattern 'initial catalog=(.*)' -AllMatches).Matches[0].Groups[1].Value
+$DBUser = ($ConnectionString -replace ';', "`n" | Select-String -Pattern 'user id=(.*)' -AllMatches).Matches[0].Groups[1].Value
+$DBPassword = ($ConnectionString -replace ';', "`n" | Select-String -Pattern 'password=(.*)' -AllMatches).Matches[0].Groups[1].Value
+
+$DBServer
+$DBUser
+$DBPassword
+$DBName
