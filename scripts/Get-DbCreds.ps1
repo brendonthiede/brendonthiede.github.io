7767ccd9dcb4bd273c8d4810c89323f126515028
Brendon Thiede <brendonthiede@filevine.com>
2024-05-28 22:14:15 -0400

    Adding

--- scripts/Get-DbCreds.ps1
+++ scripts/Get-DbCreds.ps1
@@ -15,7 +15,41 @@ $DBName = ($ConnectionString -replace ';', "`n" | Select-String -Pattern 'initia
 $DBUser = ($ConnectionString -replace ';', "`n" | Select-String -Pattern 'user id=(.*)' -AllMatches).Matches[0].Groups[1].Value
 $DBPassword = ($ConnectionString -replace ';', "`n" | Select-String -Pattern 'password=(.*)' -AllMatches).Matches[0].Groups[1].Value
 
+# if the ImportService exists, grab the import DB creds from there
+if (Test-Path -Path "C:\fv\*\*\Filevine.WindowsService.ImportService") {
+# if the SqlServer module isn't installed, install it
+    if (-NOT (Get-Module -ListAvailable -Name SqlServer)) {
+        Install-Module -Name SqlServer -Force -AllowClobber
+    }
+
+    # query the filevine database for the import database ConnecionHost
+    $SqlStatement = "SELECT TOP (1) ConnectionHost FROM [Filevine].[dbo].[ImportBatchConnection]"
+    $ImportDBServer = (Invoke-Sqlcmd -ServerInstance $DBServer -Database $DBName -Query "$SqlStatement" -Username $DBUser -Password $DBPassword -TrustServerCertificate).ConnectionHost
+
+    $ImportServiceConfig = "$((Get-ChildItem -Directory C:\fv\*\*\Filevine.WindowsService.ImportService\* | Sort-Object -Property CreationTime -Descending | Select-Object -First 1).FullName)\Filevine.WindowsService.ImportService.exe.config"
+
+    [xml]$xml = Get-Content -Path $ImportServiceConfig
+
+    $ConnectionString = ($xml.SelectSingleNode("//configuration/connectionStrings/add[@name='ImportContextMask']")).connectionString
+
+    $ImportDBName = ($ConnectionString -replace ';', "`n" | Select-String -Pattern 'initial catalog=(.*)' -AllMatches).Matches[0].Groups[1].Value
+    $ImportDBUser = ($ConnectionString -replace ';', "`n" | Select-String -Pattern 'user id=(.*)' -AllMatches).Matches[0].Groups[1].Value
+    $ImportDBPassword = ($ConnectionString -replace ';', "`n" | Select-String -Pattern 'password=(.*)' -AllMatches).Matches[0].Groups[1].Value
+
+    Write-Host "`nImport Database Credentials:"
+    $ImportDBServer
+    $ImportDBUser
+    $ImportDBPassword
+
+    Write-Host "`nImport Database Name:"
+    $ImportDBName
+}
+
+Write-Host "`nFilevine Database Credentials:"
+
 $DBServer
 $DBUser
 $DBPassword
+
+Write-Host "`nFilevine Database Name:"
 $DBName
